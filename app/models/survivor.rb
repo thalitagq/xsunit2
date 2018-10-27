a008ecfc73a5596c92b64350a8d9ddf6b073b94d
thalitagq <thalitagq@outlook.com>
2018-10-27 15:31:49 -0300

    Update survivor.rb
    identação

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,23 +1,24 @@
 class Survivor < ApplicationRecord
+   
     after_save  :check_abduction
 	after_update :check_abduction
     
-    	def check_abduction
-			reports = Survivor.find(self.id).abduction_reports
-        	survivor = Survivor.find(self.id)
+    def check_abduction
+		reports = Survivor.find(self.id).abduction_reports
+        survivor = Survivor.find(self.id)
 
-        	if reports >= 3 
-        		survivor.update_column(:abducted, "true")          
-			end		
-    	end
+        if reports >= 3 
+        	survivor.update_column(:abducted, "true")          
+		end		
+    end
     
-    	def situation
-        	total_survivors = Survivor.count
-			total_abducted = Survivor.where('abducted = true').count		
-			total_non_abducted = total_survivors - total_abducted
-			perc_abducted = (total_abducted*100)/total_survivors
-			per_non_abducted = (total_non_abducted*100)/total_survivors
-        	puts "Percentage of non-abducted survivors: #{per_non_abducted}%"
-       	 	puts "Percentage of abducted survivors: #{perc_abducted}%"
-		end
+    def situation
+		total_survivors = Survivor.count
+		total_abducted = Survivor.where('abducted = true').count		
+		total_non_abducted = total_survivors - total_abducted
+		perc_abducted = (total_abducted*100)/total_survivors
+		per_non_abducted = (total_non_abducted*100)/total_survivors
+		puts "Percentage of non-abducted survivors: #{per_non_abducted}%"
+		puts "Percentage of abducted survivors: #{perc_abducted}%"
+	end
 end
